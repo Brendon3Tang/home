dc78a46546cfa7352c25e4669aa85be62db30bdf
Brendon3Tang <kyle3tang@gmail.com>
2022-04-04 21:42:31 -0700

    Updates

---
+++ precache-manifest.33d82453ced0e7697ee16f8c22925fa5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80c5dde62fb60050a735e53b06ea2f8",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0884da5d156a9841677f",
+    "url": "/home/static/css/main.65034802.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/home/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0884da5d156a9841677f",
+    "url": "/home/static/js/main.445691f2.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32a858a59b47cd5eb48842b129667dd8",
+    "url": "/home/static/media/Long Tang.32a858a5.jpg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "d33434a974cc645ba0acf7a121cd0167",
+    "url": "/home/static/media/resume.d33434a9.pdf"
+  }
+]);
